9806d0ce7f31fe468e3409d6afb7e9998eee8d24
0404233 <andrey0404233@gmail.com>
2024-11-12 22:04:28 +0300

    feat: defined interfaces

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,41 @@
 import { Injectable } from '@nestjs/common';
 
+interface User {
+  id: string;
+  login: string;
+  password: string;
+  version: number;
+  createdAt: number;
+  updatedAt: number;
+}
+
+export interface Artist {
+  id: string;
+  name: string;
+  grammy: boolean;
+}
+
+export interface Track {
+  id: string;
+  name: string;
+  artistId: string | null;
+  albumId: string | null;
+  duration: number;
+}
+
+export interface Album {
+  id: string;
+  name: string;
+  year: number;
+  artistId: string | null;
+}
+
+export interface Favorites {
+  artists: string[];
+  albums: string[];
+  tracks: string[];
+}
+
 @Injectable()
 export class AppService {
   getHello(): string {
